6fef63142b0d6a16c01efeeffa11db649b5b7553
Samuel Davi Sponga <526898624@ist.senai>
2026-09-10 15:37:47 -0300

    v6

--- firebase.js
+++ firebase.js
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 
 const firebaseConfig = {
-    // sua configuração do Firebase
+apiKey: "AIzaSyDhtqlOgH09L8SG5cbZ1SV1tt7IRd8e394",
+authDomain: "esp32-luz-c6ad9.firebaseapp.com",
+databaseURL: "https://esp32-luz-c6ad9-default-rtdb.firebaseio.com",
+projectId: "esp32-luz-c6ad9",
+storageBucket: "esp32-luz-c6ad9.firebasestorage.app",
+messagingSenderId: "291050299879",
+appId: "1:291050299879:web:788343d4fe86d403e99d42",
+measurementId: "G-F32MEXWB9G"
 };
 
 const app = initializeApp(firebaseConfig);

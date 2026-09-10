9a7feb26a2334580c816398b89c1ce61f1f00775
Samuel Davi Sponga <526898624@ist.senai>
2026-09-10 15:39:27 -0300

    v7

--- firebase.js
+++ firebase.js
@@ -20,7 +20,7 @@ export async function enviarComando(valor) {
         await set(ref(db, "motor"), valor);
         return true;
     } catch (erro) {
-        console.error(erro);
+        console.error("Erro:", erro);
         return false;
     }
 }
